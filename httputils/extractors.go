c5f50b0d85561a3172cd96e06a11a885b83a50fb
Oleh Mushka <olegamysk@gmail.com>
2024-01-06 15:53:43 +0200

    added ExtractJWTBearerToken func to httputils

--- httputils/extractors.go
+++ httputils/extractors.go
@@ -2,7 +2,9 @@ package httputils
 
 import (
 	"context"
+	"errors"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/ecumenos/go-toolkit/contextutils"
@@ -30,4 +32,18 @@ func GetRequestDuration(ctx context.Context) (int, error) {
 	diff := time.Now().UnixNano() - start
 
 	return int(diff), nil
+}
+
+func ExtractJWTBearerToken(r *http.Request) (string, error) {
+	authHeader := r.Header.Get("Authorization")
+	if authHeader == "" {
+		return "", errors.New("authorization header is missing")
+	}
+
+	authHeaderParts := strings.Split(authHeader, " ")
+	if len(authHeaderParts) != 2 || strings.ToLower(authHeaderParts[0]) != "bearer" {
+		return "", errors.New("token prefix is missing")
+	}
+
+	return authHeaderParts[1], nil
 }
